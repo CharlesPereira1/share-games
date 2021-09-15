3fc26413b074789a71e083d3559c40e72803a8db
Charles Pereira <charlesti.ptu@gmail.com>
2021-09-14 23:52:36 -0300

    component signin style

--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -1,6 +1,10 @@
 import styled from "styled-components/native";
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
 
 export const ImageIllustration = styled.Image``;
 
