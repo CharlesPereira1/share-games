4cbf5f99a1c343c42d1b424e2091d1510e0133ee
Charles Pereira <charlesti.ptu@gmail.com>
2021-10-04 00:39:08 -0300

    component buttonIcon

--- src/components/ButtonIcon/styles.ts
+++ src/components/ButtonIcon/styles.ts
@@ -1,6 +1,14 @@
 import styled from 'styled-components/native';
+import { RFValue, RFPercentage } from 'react-native-responsive-fontsize';
 
-export const Container = styled.TouchableOpacity``;
+export const Container = styled.TouchableOpacity`
+  width: ${RFPercentage(100)};
+  height: ${RFValue(56)}px;
+  background-color: ${({ theme }) => theme.colors.primary};
+
+  flex-direction: row;
+  align-items: center;
+`;
 
 export const WrapperIcon = styled.View``;
 
