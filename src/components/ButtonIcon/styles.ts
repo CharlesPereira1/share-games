88a379f66714becbcf12a853b03d52284b111920
Charles Pereira <charlesti.ptu@gmail.com>
2021-10-06 00:14:18 -0300

    component buttonIcon

--- src/components/ButtonIcon/styles.ts
+++ src/components/ButtonIcon/styles.ts
@@ -2,14 +2,32 @@ import styled from 'styled-components/native';
 import { RFValue, RFPercentage } from 'react-native-responsive-fontsize';
 
 export const Container = styled.TouchableOpacity`
-  width: ${RFPercentage(100)};
+  width: 100%;
   height: ${RFValue(56)}px;
   background-color: ${({ theme }) => theme.colors.primary};
+  border-radius: 8px;
 
   flex-direction: row;
   align-items: center;
 `;
 
-export const WrapperIcon = styled.View``;
+export const WrapperIcon = styled.View`
+  width: 56px;
+  height: 56px;
+  justify-content: center;
+  align-items: center;
+  border-right-width: 1px;
+  border-color: ${({ theme }) => theme.colors.line};
+`;
 
-export const Icon = styled.Image``;
+export const Icon = styled.Image`
+  width: 24px;
+  height: 18px;
+`;
+
+export const Title = styled.Text`
+  flex: 1;
+  color: ${({ theme }) => theme.colors.heading};
+  font-size: ${({ theme }) => theme.size.size15};
+  text-align: center;
+`;
