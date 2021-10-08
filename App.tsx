aff7606097f937845aa892d85688d8001ac07bbd
Charles Pereira <charlesti.ptu@gmail.com>
2021-10-07 23:49:27 -0300

    fonts expo

--- App.tsx
+++ App.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
+import { useFonts } from 'expo-font';
+import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import {
+  Rajdhani_500Medium,
+  Rajdhani_700Bold,
+} from '@expo-google-fonts/rajdhani';
 
 import SignIn from './src/screens/SignIn';
 
