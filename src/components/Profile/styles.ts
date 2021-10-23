cb4edcb197bb9f0541e4d879557a48722c1efc18
Charles Pereira <charlesti.ptu@gmail.com>
2021-10-22 23:50:29 -0300

    component profile

--- src/components/Profile/styles.ts
+++ src/components/Profile/styles.ts
@@ -1,12 +1,22 @@
+import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex-direction: row;
+`;
 
 export const Wrapper = styled.View``;
 
-export const User = styled.View``;
+export const User = styled.View`
+  flex-direction: row;
+`;
 
-export const Greeting = styled.Text``;
+export const Greeting = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.title500};
+  font-size: ${RFValue(24)}px;
+  color: ${({ theme }) => theme.colors.heading};
+  margin-right: 5;
+`;
 
 export const UserName = styled.Text``;
 
