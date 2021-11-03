1cc0170a8d67fea9fbb7725b9dae4c0231d81ab7
Charles Pereira <charlesti.ptu@gmail.com>
2021-11-02 23:49:51 -0300

    routes index

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -8,7 +8,14 @@ const { Navigator, Screen } = createStackNavigator();
 
 const AuthRoutes: React.FC = () => {
   return (
-    <Navigator>
+    <Navigator
+      headerMode="none"
+      screenOptions={{
+        cardStyle: {
+          backgroundColor: 'transparent',
+        },
+      }}
+    >
       <Screen name="SignIn" component={SignIn} />
       <Screen name="Home" component={Home} />
     </Navigator>
